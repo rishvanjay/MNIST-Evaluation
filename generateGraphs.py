7fd7812137a4598cf9726d21deb2cd8c672231ba
rishvanjay <rishvanjay@hotmail.com>
2018-12-01 19:18:08 -0500

    generate plots

--- generateGraphs.py
+++ generateGraphs.py
@@ -5,59 +5,33 @@ import final
 
 X_train, X_test, y_train, y_test = final.getdata()
 
-# resEp1 = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 0.5)
-# resEp5 = final.run(X_train, X_test, y_train, y_test, 5, 0.01, 0.5)
-# resEp10 = final.run(X_train, X_test, y_train, y_test, 10, 0.01, 0.5)
-# resEp100 = final.run(X_train, X_test, y_train, y_test, 100, 0.01, 0.5)
 
-
-# resPen001 = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 0.01)
-# resPen01 = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 0.1)
-# resPen1 = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 1)
-# resPen10 = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 10)
-
-
-# resGam001 = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 0.01)
-# resGam01 = final.run(X_train, X_test, y_train, y_test, 1, 0.1, 0.01)
-# resGam1 = final.run(X_train, X_test, y_train, y_test, 1, 1, 0.01)
-
-resCompDigits = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 0.5)
 
 # accuracy vs hyperparameter graphs
 
-
 #comparing number of epochs with overall accuracy
 
-# res1 = np.mean(resEp100[0])
-# res5 = np.mean(resEp100[4])
-# res10 = np.mean(resEp100[9])
-# res50 = np.mean(resEp100[49])
-# res100 = np.mean(resEp100[99])
-# y = [res1, res5, res10, res50, res100]
-# x = [1, 5, 10, 50, 100]
-# y = []
-# x = []
-# for i, l in enumerate(resEp100):
-#     y.append(np.mean(resEp100[i]))
-#     x.append(i+1)
 
-# pyplot.plot(x, y)
-# pyplot.title('Number of Epochs vs Overall Accuracy')
-# pyplot.xlabel('Number of Epochs')
-# pyplot.ylabel('Accuracy')
-# pyplot.ylim(0.94, 0.98)
-# pyplot.show()
+resEp100 = final.run(X_train, X_test, y_train, y_test, 100, 0.01, 0.5)
+
+
+y = []
+x = []
+for i, l in enumerate(resEp100):
+    y.append(np.mean(resEp100[i]))
+    x.append(i+1)
+
+pyplot.plot(x, y)
+pyplot.title('Number of Epochs vs Overall Accuracy')
+pyplot.xlabel('Number of Epochs')
+pyplot.ylabel('Accuracy')
+pyplot.ylim(0.94, 0.98)
+pyplot.show()
 
 
 #comparing overall accuracy with different penalty  
 
-# resPen001 = np.mean(resPen001)
-# resPen01 = np.mean(resPen01)
-# resPen1 = np.mean(resPen1)
-# resPen10 = np.mean(resPen10)
 
-# y = [resPen001, resPen01, resPen1, resPen10]
-# x = [0.01, 0.1, 1, 10]
 
 y = []
 x = []
@@ -77,11 +51,7 @@ pyplot.show()
 
 #comparing overall accuracy with different gamma
 
-# resGam001 = np.mean(resGam001)
-# resGam01 = np.mean(resGam01)
-# resGam1 = np.mean(resGam1)
-# y = [resGam001, resGam01, resGam1]
-# x = [0.01, 0.1, 1]
+
 
 y = []
 x = []
@@ -101,9 +71,12 @@ pyplot.show()
 
 #compare which digits train more accurately
 
+
+resCompDigits = final.run(X_train, X_test, y_train, y_test, 1, 0.01, 0.5)
 y = resCompDigits[0]
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-pyplot.plot(x, y)
+pyplot.bar(x, y)
+pyplot.ylim(0.85, 1)
 pyplot.title('Digit vs Training Accuracy')
 pyplot.xlabel('Digit')
 pyplot.ylabel('Accuracy')
